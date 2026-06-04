1deaf216ddab7e2949fb9119ce9a121e8d915095
James Ly <jamesquietly@gmail.com>
2026-06-04 16:18:55 -0700

    update create plan btn

--- features/plan/components/PlanDialog.tsx
+++ features/plan/components/PlanDialog.tsx
@@ -1,13 +1,30 @@
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { CreatePlan } from "./CreatePlan";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import { CreatePlan } from './CreatePlan';
 
-export function PlanDialog({ showTrigger, open }: { showTrigger?: boolean; open?: boolean }) {
+export function PlanDialog({
+  showTrigger,
+  open,
+}: {
+  showTrigger?: boolean;
+  open?: boolean;
+}) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <Dialog open={open ?? isOpen} onOpenChange={setIsOpen}>
-      {showTrigger && <DialogTrigger render={<Button>Create Plan</Button>}></DialogTrigger>}
+      {showTrigger && (
+        <DialogTrigger
+          render={<Button className="text-2xl py-5">Create Plan</Button>}
+        ></DialogTrigger>
+      )}
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Create Plan</DialogTitle>
